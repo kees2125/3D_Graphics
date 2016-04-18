bc7e3ec15376ca56a1a1aa623fa45922f241fd08
Aareschluchtje <remcosannen@gmail.com>
2016-04-18 16:04:21 +0200

    Fixed cube

--- 3d_graphics/3d_graphics/Main.cpp
+++ 3d_graphics/3d_graphics/Main.cpp
@@ -21,7 +21,7 @@ int main()
 {
 	init();
 
-	//int polygonindexes[6][4] = { {0,1,2,3}, {4,5,6,7}, {0,1,4,5}, ;
+	int polygonindexescube[6][4] = { {0,1,2,3}, {4,5,6,7}, {0,1,5,4}, {1,2,6,5}, {2,3,7,6}, {3,0,4,7} };
 
 
 	vector a = vector(-1, -1, -1);
@@ -36,8 +36,7 @@ int main()
 	vector list[8] = { a,b,c,d,e,f,g,h };
 
 	Matrix perspect = Matrix::Perspective(90, (6 / 8.0f), 0.1, 20);
-<<<<<<< HEAD
-	Matrix translate = Matrix::Translate(vector(5, 0, 5));
+	Matrix translate = Matrix::Translate(vector(4, 0, 4));
 	//Matrix rotatie = Matrix::Rotate(vector(1, 1, 1), ALLEGRO_PI / 5);
 
 	for (int i = 0; i < 8; i++)
@@ -47,44 +46,20 @@ int main()
 		//list[i] = rotatie.MultiplyByVector(list[i]);
 
 	}
-=======
-	Matrix translate = Matrix::Translate(vector(1, 1, 1));
-	Matrix rotatie = Matrix::Rotate(vector(1, 1, 1), ALLEGRO_PI / 2);
->>>>>>> aef60a180e3a05f00e8338330b2875c8d32a795b
-	
 	int temp = 1;
 	while (true)
 	{
-		
-<<<<<<< HEAD
 		ALLEGRO_COLOR color = ALLEGRO_COLOR();
 		color.g = 255;
 		//al_draw_line(1.0f, 1.0f, 200.0f, 200.0f, color , 1.0f);
 		
-		for (int i = 0; i < 8; i ++)
+		for (int i = 0; i < 6; i ++)
 		{
-			al_draw_line(160 + list[i].x /list[i].z * 150, 210 + list[i].y / list[i].z * 200, 160 + list[i + 1].x / list[i + 1].z * 150, 210 + list[i + 1].y / list[i + 1].z * 200, color , 1.0f);
-=======
-		temp++;
-		if (temp > 10)
-		{
-			temp = 1;
-		}
-		vector listTemp[8];
-		for (int i = 0; i < 8;i++)
-		{
-			listTemp[i] = perspect.MultiplyByVector(list[i]);
-			listTemp[i] = translate.MultiplyByVector(list[i]);
-			listTemp[i] = rotatie.MultiplyByVector(list[i]);
-			
-		}
-		ALLEGRO_COLOR at = ALLEGRO_COLOR();
-		at.g = 255;
-		//al_draw_line(1.0f, 1.0f, 200.0f, 200.0f, a , 1.0f);
-		for (int i = 0; i < 8; i += 2)
-		{
-			al_draw_line( listTemp[i].x/listTemp[i].z +400 * 400, listTemp[i].y/listTemp[i].z+300 *300, listTemp[i++].x / listTemp[i++].z + 400 * 400, listTemp[i++].y / listTemp[i++].z + 300 * 300,at,1.0f);
->>>>>>> aef60a180e3a05f00e8338330b2875c8d32a795b
+			for (int p = 0; p < 3; p++)
+			{
+				al_draw_line(160 + list[polygonindexescube[i][p]].x / list[polygonindexescube[i][p]].z * 150, 210 + list[polygonindexescube[i][p]].y / list[polygonindexescube[i][p]].z * 200, 160 + list[polygonindexescube[i][p + 1]].x / list[polygonindexescube[i][p + 1]].z * 150, 210 + list[polygonindexescube[i][p + 1]].y / list[polygonindexescube[i][p + 1]].z * 200, color, 1.0f);
+			}
+			al_draw_line(160 + list[polygonindexescube[i][3]].x / list[polygonindexescube[i][3]].z * 150, 210 + list[polygonindexescube[i][3]].y / list[polygonindexescube[i][3]].z * 200, 160 + list[polygonindexescube[i][0]].x / list[polygonindexescube[i][0]].z * 150, 210 + list[polygonindexescube[i][0]].y / list[polygonindexescube[i][0]].z * 200, color, 1.0f);
 		}
 		//al_draw_line(listTemp[0].x, listTemp[0].y, listTemp[7].x, listTemp[7].y, a, 1.0f);
 		al_flip_display();
